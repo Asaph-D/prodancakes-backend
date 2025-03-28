a58ab560494daf5111ea8215e5deb3c22956cac6
Asaph-D <kouokamasaph142@gmail.com>
2025-03-28 12:09:51 +0100

    cors config

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 // Configurer le stockage des images
